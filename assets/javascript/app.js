702f482a1edf50d557cd6bcd20f278df2c4810b7
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-08 21:07:41 -0700

    Call resetGame function and also create start btn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,60 @@
 // https://www.classicfm.com/discover-music/latest/ultimate-classical-music-quiz/
 
-const triviaGame = {
-  gameState: null,
+$(document).ready(function () {
+  const htmlContent = $('#content');
 
-  qAndA: [
-    {
-      question: 'Which of these is NOT the subtitle of a Haydn symphony?',
-      answers: {
-        a1: 'Hornsignal',
-        a2: 'Lamentatione',
-        a3: 'Tragic',
-        a4: 'Mercury'
+  const triviaGame = {
+    gameState: null,
+
+    qAndA: [
+      {
+        question: 'Which of these is NOT the subtitle of a Haydn symphony?',
+        answers: {
+          a1: 'Hornsignal',
+          a2: 'Lamentatione',
+          a3: 'Tragic',
+          a4: 'Mercury'
+        },
+        correctAns: 'a3'
       },
-      correctAns: 'a3'
+      {
+        question: 'Which of Elgar\'s Enigma Variations was partially inspired by a bulldog?',
+        answers: {
+          a1: 'Variation XI (G.R.S.)',
+          a2: 'Variation I (C.A.E.)',
+          a3: 'Variation XII (B.G.N.)',
+          a4: 'Variation IX (Nimrod)'
+        },
+        correctAns: 'a1'
+      },
+      {
+        question: 'Where was cellist Jaqueline du Pré born?',
+        answers: {
+          a1: 'Cheltenham',
+          a2: 'Oxford',
+          a3: 'Cambridge',
+          a4: 'Cirencester'
+        },
+        correctAns: 'a2'
+      }
+    ],
+
+    resetGame: function () {
+      this.gameState = this.gameStateFactory();
     },
-    {
-      question: 'Which of Elgar\'s Enigma Variations was partially inspired by a bulldog?',
-      answers: {
-        a1: 'Variation XI (G.R.S.)',
-        a2: 'Variation I (C.A.E.)',
-        a3: 'Variation XII (B.G.N.)',
-        a4: 'Variation IX (Nimrod)'
-      },
-      correctAns: 'a1'
-    },
-    {
-      question: 'Where was cellist Jaqueline du Pré born?',
-      answers: {
-        a1: 'Cheltenham',
-        a2: 'Oxford',
-        a3: 'Cambridge',
-        a4: 'Cirencester'
-      },
-      correctAns: 'a2'
-    }
-  ],
 
-  resetGame: function () {
-    this.gameState = this.gameStateFactory();
-  },
-
-  gameStateFactory: function () {
-    return {
-      remainTime: 30000,
-      correct: 0,
-      incorrect: 0
+    gameStateFactory: function () {
+      return {
+        remainTime: 30000,
+        correct: 0,
+        incorrect: 0
+      }
     }
+  }; // =========== triviaGame END =============
+
+  if (!triviaGame.gameState){
+    triviaGame.resetGame();
   }
-}
+
+
+});
