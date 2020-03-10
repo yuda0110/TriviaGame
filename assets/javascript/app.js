4194e787c832f12241f79731bea95a0a4de6792a
Yuko Uda <yuko.uda0110@gmail.com>
2020-03-09 23:12:28 -0700

    Create showQnA methods to show a new question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,7 +52,7 @@
         correct: 0,
         incorrect: 0,
         clockRunning: false,
-        gameNum: 0
+        questionNum: 0
       }
     },
 
@@ -69,7 +69,6 @@
     },
 
     countTime: function () {
-      console.log('countTime gameState: ' + triviaGame.gameState);
       if (triviaGame.gameState.remainTime > 0) {
         triviaGame.gameState.remainTime = triviaGame.gameState.remainTime - 1;
         $('#current-time').text(triviaGame.gameState.remainTime);
@@ -78,8 +77,45 @@
       }
     },
 
+    showQnA: function () {
+      const currentQA = triviaGame.qAndA[triviaGame.gameState.questionNum];
+      triviaGame.clockStart();
+      htmlContent.empty();
+      triviaGame.appendTimeRemaining();
+      triviaGame.appendQuestion(currentQA);
+      triviaGame.appendAnswerChoices(currentQA);
+    },
+
+    appendTimeRemaining: function () {
+      const timeRemain = $('<p>');
+      timeRemain.addClass('time-remain');
+      timeRemain.html(`Time Remaining: <span id="current-time">${this.gameState.remainTime}</span>Seconds`);
+      htmlContent.append(timeRemain);
+    },
+
+    appendQuestion: function (currentQA) {
+      const question = $('<p>');
+      question.addClass('question');
+      question.text(`${currentQA.question}`);
+      htmlContent.append(question);
+    },
+
+    appendAnswerChoices: function (currentQA) {
+      const answersContainer = $('<div>');
+      answersContainer.addClass('answers');
+      for (const property in currentQA.answers) {
+        console.log(`${property}: ${currentQA.answers[property]}`);
+        const answer = $('<button>');
+        answer.attr('id', property);
+        answer.addClass('answer-btn');
+        answer.text(currentQA.answers[property]);
+        answersContainer.append(answer);
+      }
+      htmlContent.append(answersContainer);
+    },
+
     addGameNum: function () {
-      this.gameState.gameNum++;
+      this.gameState.questionNum++;
     }
 
   }; // =========== triviaGame END =============
@@ -89,36 +125,13 @@
     triviaGame.resetGame();
   }
 
-  const currentQA = triviaGame.qAndA[triviaGame.gameState.gameNum];
-
   $('#start').on('click', function () {
-    triviaGame.clockStart();
-    if (triviaGame.gameState.remainTime < 0) {
-      triviaGame.clockStop();
-    }
-    const timeRemain = $('<p>');
-    timeRemain.addClass('time-remain');
-    timeRemain.html(`Time Remaining: <span id="current-time">${triviaGame.gameState.remainTime}</span>Seconds`);
-
-    const question = $('<p>');
-    question.addClass('question');
-    question.text(`${currentQA.question}`);
-
-    const answersContainer = $('<div>');
-    answersContainer.addClass('answers');
-    for (const property in currentQA.answers) {
-      console.log(`${property}: ${currentQA.answers[property]}`);
-      const answer = $('<button>');
-      answer.attr('id', property);
-      answer.addClass('answer-btn');
-      answer.text(currentQA.answers[property]);
-      answersContainer.append(answer);
-    }
-
-    htmlContent.empty().append(timeRemain, question, answersContainer);
+    triviaGame.showQnA();
   });
 
   $(document).on('click', '.answer-btn', function () {
+    const currentQA = triviaGame.qAndA[triviaGame.gameState.questionNum];
+
     triviaGame.clockStop();
     $('.question').remove();
     $('.answers').remove();
@@ -128,11 +141,17 @@
     if (this.id === currentQA.correctAns) {
       messageEl.text('Correct!');
       htmlContent.append(messageEl);
+
+      triviaGame.addGameNum();
+      setTimeout(triviaGame.showQnA, 4000);
     } else { // if the player clicks a wrong answer
       messageEl.text('Nope!');
       const correctAnsEl = $('<p>');
       correctAnsEl.text(`The Correct Answer was: ${currentQA.answers[currentQA.correctAns]}`);
       htmlContent.append(messageEl, correctAnsEl);
+
+      triviaGame.addGameNum();
+      setTimeout(triviaGame.showQnA, 4000);
     }
   });
 
